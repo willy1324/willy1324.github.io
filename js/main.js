569c4e26c6e645e4bdf0c814772f83d2f46b4e27
willy1324 <thrasherx007@gmail.com>
2024-07-14 14:54:27 -0400

    mas bugs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,66 @@
-import { randomAgent } from "./characterDb";
+export class randomAgent 
+{
+    constructor()
+    {
+            this.controller = 
+        [
+            "Brimstone",
+            "Astra",
+            "Clove",
+            "Harbor",
+            "Omen",
+            "Viper"
+        ];
+
+        this.initiator = 
+        [
+            "Breach",
+            "Fade",
+            "Gekko",
+            "KAY/O",
+            "Skye",
+            "Sova"
+        ];
+
+        this.sentinel = 
+        [
+            "Chamber",
+            "Cypher",
+            "Deadlock",
+            "Killjoy",
+            "Sage"
+        ];
+
+        this.duelist = 
+        [   
+            "Iso",
+            "Jett",
+            "Neon",
+            "Phoenix",
+            "Raze",
+            "Reyna",
+            "Yoru"
+        ];
+
+        this.juampaPunisher = 
+        [
+            "Yoru",
+            "Astra",
+            "Brimstone",
+            "Deadlock"
+        ];
+
+        this.roleList = 
+        [
+            "Controlador",
+            "Iniciador",
+            "Centinela",
+            "Duelista",
+            //this.juampaPunisher
+        ];
+    }
+
+}
 
 playerNames = ["Juampa", "Lucía", "Fabri", "Pancho", "Juguito"]
 let characters = new randomAgent();
