84f9a6c0af0ce40d28d00bffce16ec4804b9a914
Wilians <thrasherx007@gmail.com>
2025-01-10 19:15:09 -0300

    agregado logo catFinder

--- movieDb/scripts/main.js
+++ movieDb/scripts/main.js
@@ -1,1 +1,11 @@
-axiosApi = axios.create({ baseURL: "https://api.themoviedb.org" });
+axiosApi = axios.create({ baseURL: "https://api.themoviedb.org/3/" });
+
+async function getTrendingMoviesPreview() {
+  const response = await fetch(
+    "https://api.themoviedb.org/3/trending/movie/day?api_key=" + MOVIE_DB_KEY
+  );
+  const data = await response.json();
+  const movies = data.results;
+
+  movies.forEach((movie) => {});
+}
